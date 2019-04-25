80f210750416347464101220cbe4866f5793dd82
Luciano Buono <luciano.buono@outlook.com>
2019-04-25 11:24:51 -0300

    Stuck on Step 8 working (img sent throgh app is working but the app only show .gif) Step9 is omitted.

---
+++ build-android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MyFirebaseMessagingService.java
@@ -0,0 +1,36 @@
+package com.google.firebase.codelab.friendlychat;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    private static final String TAG = "MyFMService";
+
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle data payload of FCM messages.
+        Log.d(TAG, "FCM Message Id: " + remoteMessage.getMessageId());
+        Log.d(TAG, "FCM Notification Message: " +
+                remoteMessage.getNotification());
+        Log.d(TAG, "FCM Data Message: " + remoteMessage.getData());
+    }
+
+    private static final String TAG2 = "MyFirebaseIIDService";
+    private static final String FRIENDLY_ENGAGE_TOPIC = "friendly_engage";
+
+    public void onNewToken() {
+        // If you need to handle the generation of a token, initially or
+        // after a refresh this is where you should do that.
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG2, "FCM Token: " + token);
+
+        // Once a token is generated, we subscribe to topic.
+        FirebaseMessaging.getInstance()
+                .subscribeToTopic(FRIENDLY_ENGAGE_TOPIC);
+    }
+}
